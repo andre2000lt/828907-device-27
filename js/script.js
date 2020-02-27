92cd9a39863f63e2866bde940d6f33d57d0cc9a4
Andre <info@pcgames.lt>
2020-02-27 11:42:38 +0200

    Поправки

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
-// Переулючаем слайды сервисов
+// ----- Переключаем слайды сервисов -----
+
 var services_buttons = document.querySelectorAll(".services-button");
 var services_list = document.querySelector(".services-list");
 
@@ -45,3 +46,140 @@ for (var i = 0; i < services_buttons.length; i++)
 {
     addCurrentClass(i);
 }
+
+// (КОНЕЦ) -----  Переключаем слайды сервисов -----
+
+
+
+// ----- Открытие / закрытие модального окна .write-us-window -----
+
+var isStorageSupport = true;
+var saved_name = "";
+var saved_email = "";
+try
+{
+  saved_name = localStorage.getItem("user_name");
+  saved_email = localStorage.getItem("user_email");
+}
+catch (err)
+{
+  isStorageSupport = false;
+}
+
+
+var write_us_link = document.querySelector(".write-us-link");
+
+var write_us_window = document.querySelector(".write-us-window");
+var close_write_us_window = write_us_window.querySelector(".window-close-button");
+
+var name_field = write_us_window.querySelector("[name=name]");
+var email_field = write_us_window.querySelector("[name=email]");
+var message_field = write_us_window.querySelector("[name=message]");
+
+var write_us_form = write_us_window.querySelector("form");
+
+
+// Открытие модального окна, фокусировка и авто заполнение полей
+write_us_link.addEventListener("click", function(evt)
+{
+    evt.preventDefault();
+    write_us_window.classList.add("modal-window-visible");
+
+    var focused_field = name_field;
+    if ( isStorageSupport )
+    {
+        if ( saved_name )
+        {
+            name_field.value = saved_name;
+            focused_field = email_field;
+
+            if ( email_field )
+            {
+                email_field.value = saved_email;
+                focused_field = message_field;
+            }
+        }
+    }
+
+    focused_field.focus();
+});
+
+
+// Закрытие модального окно по нажатию крестика
+
+function hideWindowByClick(close_button, modal_window)
+{
+    close_button.addEventListener("click", function(evt)
+    {
+        modal_window.classList.remove("modal-window-visible");
+    });
+}
+
+hideWindowByClick(close_write_us_window, write_us_window);
+
+
+// Закрытие модального окно по нажатию ESC
+function hideWindowByEsc(modal_window)
+{
+    window.addEventListener("keydown", function(evt)
+    {
+        if ( evt.keyCode === 27)
+        {
+            evt.preventDefault();
+            if( modal_window.classList.contains("modal-window-visible") )
+            {
+                modal_window.classList.remove("modal-window-visible");
+            }
+        }
+    });
+}
+
+hideWindowByEsc(write_us_window);
+
+
+// Проверка заполнения полей
+write_us_form.addEventListener("submit", function (evt)
+{
+    var invalid_fields = 0;
+    var fields = [name_field, email_field, message_field];
+    for (var i = 0; i < fields.length; i++ )
+    {
+        if( !fields[i].value )
+        {
+            fields[i].classList.add("field-invalid");
+            invalid_fields++;
+        }
+    }
+
+    if ( invalid_fields )
+    {
+        evt.preventDefault();
+        console.log("Заполните все поля");
+    }
+    else
+    {
+        localStorage.setItem("user_name", name_field.value);
+        localStorage.setItem("user_email", email_field.value);
+    }
+});
+
+// (КОНЕЦ) ----- Открытие / закрытие модального окна .write-us-window -----
+
+
+
+// ----- Открытие / закрытие модального окна карты -----
+
+var map_link = document.querySelector(".map-link");
+var map_window = document.querySelector(".map-window");
+var close_map_window = map_window.querySelector(".window-close-button");
+
+
+// Открытие модального окна карты
+map_link.addEventListener("click", function(evt)
+{
+    evt.preventDefault();
+    map_window.classList.add("modal-window-visible");
+});
+
+hideWindowByClick(close_map_window, map_window);
+hideWindowByEsc(map_window);
